8a5778393f0ceafdaf0be23f0b7b543e3244383d
Lyove <lyove@outlook.com>
2024-06-07 17:28:31 +0800

    init

--- platform/themes/ripple/partials/breadcrumbs.blade.php
+++ platform/themes/ripple/partials/breadcrumbs.blade.php
@@ -1,4 +1,11 @@
-<section class="section page-intro pt-50 pb-50 bg-cover" data-background="{{ Theme::get('breadcrumbBannerImage') ?: (theme_option('default_breadcrumb_banner_image') ? RvMedia::getImageUrl(theme_option('default_breadcrumb_banner_image')) : Theme::asset()->url('images/page-intro-02.jpg')) }}">
+@php
+$breadcrumb_image = Theme::get('breadcrumbBannerImage') ?: (theme_option('default_breadcrumb_banner_image') ? RvMedia::getImageUrl(theme_option('default_breadcrumb_banner_image')) : Theme::asset()->url('images/page-intro-02.jpg'));
+@endphp
+
+<section 
+    class="section page-intro pt-50 pb-50 bg-cover" 
+    data-background="{{ $breadcrumb_image }}"
+>
     <div style="opacity: 0.7" class="bg-overlay"></div>
     <div class="container">
         <h3 class="page-intro__title">{{ Theme::get('section-name') ?: SeoHelper::getTitle() }}</h3>
